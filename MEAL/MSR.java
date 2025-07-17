3a01f2bc83f2e814b9e0bbeedfb26060abc6eea0
CPMTsut <stevendoctorliu@gmail.com>
2025-07-17 15:16:51 +0800

    Add files via upload

---
+++ MEAL/MSR.java
@@ -0,0 +1,11 @@
+package mutants.MOS;
+
+public class MSR {
+	public int numberOfFirstClassMeals;	
+	public int numberOfBusinessClassMeals;	
+	public int numberOfEconomicClassMeals;
+	public int numberOfMealsForCrewMembers;
+	public int numberOfMealsForPilots;
+	public int numberOfChildMeals;
+	public int numberOfBundlesOfFlowers;
+}
